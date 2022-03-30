57de66c3f98013da26e6ad393768b0727e34ba2c
Gerardo Wacker <ge@rar.vg>
2022-03-30 20:49:20 -0300

    Added database creation into index file, prevented the creation of 2 separate objects

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@
 const Web = require('./managers/web.manager')
 const Socket = require('./managers/socket.manager')
 const Session = require('./managers/session.manager')
+const Database = require('./managers/database.manager')
 
-const socket = new Socket()
 const session = new Session()
-const web = new Web()
+const database = new Database(session)
+const socket = new Socket(database, session)
+const web = new Web(database, session)
 
 web.start(1301).then(() => console.log('🚀 Servidor web iniciado en el puerto 1301.'))
 socket.start(1302, session).then(() => console.log('📦 Servidor de sockets iniciado en el puerto 1302.'))
