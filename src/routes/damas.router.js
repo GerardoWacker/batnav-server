585f0a6e0e1070280e33698a1594278b8ed8276b
Gerardo Wacker <ge@rar.vg>
2022-03-30 20:48:37 -0300

    Converted Router into a JS Class

--- src/routes/damas.router.js
+++ src/routes/damas.router.js
@@ -1,6 +1,36 @@
 const express = require('express')
-const router = express.Router()
 
-router.get('/', (req, res) => res.send('La curiosidad mató al gato.'))
+class Router
+{
+    /**
+     * Router principal
+     * @param database Base de datos.
+     */
+    constructor(database)
+    {
+        this.database = database
+    }
 
-module.exports = router
+    /**
+     * Crea una instancia de Router.
+     * @returns {Promise<Router>}
+     */
+    create()
+    {
+        return new Promise(res =>
+        {
+            const router = express.Router()
+            this.database.connect().then(() =>
+            {
+                router.get('/', (req, res) => res.send('La curiosidad mató al gato.'))
+                router.post('/login', (req, res) => this.database.login(req.body).then(result => res.send(result)))
+                router.post('/register', (req, res) => this.database.register(req.body).then(result => res.send(result)))
+                router.post('/user/:id', (req, res) => this.database.getUserById(req.params.id).then(result => res.send(result)))
+            })
+            res(router)
+        })
+    }
+
+}
+
+module.exports = Router
