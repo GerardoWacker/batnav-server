07fef79cb4b11b36591ca6700b454880756ef6c4
Gerardo Wacker <ge@rar.vg>
2022-03-30 20:48:10 -0300

    Added constructor into Socket manager

--- src/managers/socket.manager.js
+++ src/managers/socket.manager.js
@@ -1,16 +1,20 @@
 const { Server } = require('socket.io')
-const Database = require('./database.manager')
-const database = new Database()
 
 class Socket {
+    constructor(database, sessionManager)
+    {
+        this.sessionManager = sessionManager
+        this.database = database
+    }
+
     io = new Server()
 
     /** Estructura -> UUID Identificador de sesión : ID de socket */
     playerPool = new Map()
 
-    start(port, sessionManager) {
-        return new Promise((res, rej) => {
-            database.connect()
+    start(port) {
+        return new Promise(res => {
+            this.database.connect()
                 .then(() => console.log('📝 (Socket) Conexión con base de datos establecida.'))
                 .catch(err => {
                     console.log('❌ Ocurrió un error al conectarse a la base de datos.')
@@ -19,18 +23,19 @@ class Socket {
             
             this.io.on('connection', socket => {
                 console.log('⬇️ (Socket) ¡Conexión entrante!')
-                socket.on('authenticate', uuid => this.authenticate(socket, sessionManager, uuid))
+                socket.on('authenticate', uuid => this.authenticate(socket, uuid))
             })
             this.io.listen(port)
             res(this.io)
         })
     }
 
-    authenticate(socket, sessionManager, uuid) {
-        sessionManager.validate(uuid).then(response => {
+    authenticate(socket, uuid) {
+        this.sessionManager.validate(uuid).then(response => {
             if(response.success) {
-                database.getUserById(response.content).then(user => {
+                this.database.getUserById(response.content).then(user => {
                     socket.emit('authentication', {success: true, content: user})
+                    this.playerPool.set(uuid, socket.id)
                     this.io.emit('user-connection', user.username)
                     console.log('⬆️ (Socket) ¡Emitido paquete!')
                 })
