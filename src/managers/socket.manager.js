2fd0ec68de68f3f66ef9b5fb3cc68b60cea6d01d
Gerardo Wacker <ge@rar.vg>
2022-03-30 00:50:41 -0300

    Added Socket manager

---
+++ src/managers/socket.manager.js
@@ -0,0 +1,43 @@
+const { Server } = require('socket.io')
+const Database = require('./database.manager')
+const database = new Database()
+
+class Socket {
+    io = new Server()
+
+    /** Estructura -> UUID Identificador de sesión : ID de socket */
+    playerPool = new Map()
+
+    start(port, sessionManager) {
+        return new Promise((res, rej) => {
+            database.connect()
+                .then(() => console.log('📝 Conexión con base de datos establecida (Socket).'))
+                .catch(err => {
+                    console.log('❌ Ocurrió un error al conectarse a la base de datos.')
+                    console.error(err)
+                })
+            
+            this.io.on('connection', socket => {
+                console.log('⬇️ (Socket) ¡Conexión entrante!')
+                socket.on('authenticate', uuid => this.authenticate(socket, sessionManager, uuid))
+            })
+            this.io.listen(port)
+            res(this.io)
+        })
+    }
+
+    authenticate(socket, sessionManager, uuid) {
+        sessionManager.validate(uuid).then(response => {
+            if(response.success) {
+                database.getUserById(response.content).then(user => {
+                    socket.emit('authentication', {success: true, content: user})
+                    this.io.emit('user-connection', user.username)
+                    console.log('⬆️ (Socket) ¡Emitido paquete!')
+                })
+            } else
+                socket.emit('authentication', {success: false, content: "Hubo un error validando la sesión: " + response.content})
+        })
+    }
+}
+
+module.exports = Socket
