5b4d80c162764efdce6a879eab4e897dd34b50ee
Gerardo Wacker <ge@rar.vg>
2022-03-30 18:53:25 -0300

    Reformatted Session manager

--- src/managers/session.manager.js
+++ src/managers/session.manager.js
@@ -1,29 +1,37 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 
-class Session {
+class Session
+{
     /** Estructura -> ID de usuario : UUID Identificador de sesión */
     openSessions = new Map()
 
-    create(userID) {
-        return new Promise((res, rej) => {
+    create(userID)
+    {
+        return new Promise((res, rej) =>
+        {
             let uuid = uuid()
             this.openSessions.set(userID, uuid)
             res({success: true, content: uuid})
         })
     }
 
-    validate(uuid) {
-        return new Promise((res, rej) => {
-            if(Array.from(this.openSessions.values).includes(uuid)) {
-                for (let [userID, uuid_] of this.openSessions.entries()) {
+    validate(uuid)
+    {
+        return new Promise((res, rej) =>
+        {
+            if (Array.from(this.openSessions.values()).includes(uuid))
+            {
+                for (let [userID, uuid_] of this.openSessions.entries())
+                {
                     if (uuid_ === uuid)
-                    res({success: true, content: userID})
+                        res({success: true, content: userID})
                 }
-            } else
+            }
+            else
                 res({success: false, content: "La sesión solicitada es inválida, o ha expirado."})
         })
     }
-    
+
 }
 
 module.exports = Session
