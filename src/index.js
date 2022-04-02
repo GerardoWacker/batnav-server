714865376e75ba2b6a2399c37a514107f5519c62
Gerardo Wacker <ge@rar.vg>
2022-04-01 21:32:49 -0300

    Added configuration files for port selection

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@
  * @author Gerardo Wacker
  */
 
-const Web = require('./managers/web.manager')
-const Socket = require('./managers/socket.manager')
-const Session = require('./managers/session.manager')
-const Database = require('./managers/database.manager')
+const config = require("../config/config.json");
 
-const session = new Session()
-const database = new Database(session)
-const socket = new Socket(database, session)
-const web = new Web(database, session)
+const Web = require("./managers/web.manager");
+const Socket = require("./managers/socket.manager");
+const Session = require("./managers/session.manager");
+const Database = require("./managers/database.manager");
 
-web.start(1301).then(() => console.log('🚀 Servidor web iniciado en el puerto 1301.'))
-socket.start(1302).then(() => console.log('📦 Servidor de sockets iniciado en el puerto 1302.'))
+const session = new Session();
+const database = new Database(session);
+const socket = new Socket(database, session);
+const web = new Web(database, session);
+
+web
+  .start(config.port.web)
+  .then(() => console.log("🚀 Servidor web iniciado en el puerto 1301."));
+socket
+  .start(config.port.socket)
+  .then(() =>
+    console.log("📦 Servidor de sockets iniciado en el puerto 1302.")
+  );
+
