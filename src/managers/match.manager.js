558f0e2b0be6e5ec027be05c5d84c5d7bfb1c583
Gerardo Wacker <ge@rar.vg>
2022-04-26 08:25:23 -0300

    Added check for ship setting array size (`MatchManager::setShips`)

--- src/managers/match.manager.js
+++ src/managers/match.manager.js
@@ -1,206 +1,221 @@
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-class Match
-{
-    /** Structure -> Match Id. : {
-     * player1: {
-     *     id: Session Id.,
-     *     ships: [
-     *         [Ship's occupied points in coordinates format]
-     *     ],
-     *     bombs: Thrown bombs.
-     * },
-     * player2: {
-     *     id: Session Id.,
-     *     ships: [
-     *         [Ship's occupied points in coordinates format]
-     *     ],
-     *     bombs: Thrown bombs.
-     * }
-     * }*/
-    currentMatches = new Map()
+class Match {
+  /** Structure -> Match Id. : {
+   * player1: {
+   *     id: Session Id.,
+   *     ships: [
+   *         [Ship's occupied points in coordinates format]
+   *     ],
+   *     bombs: Thrown bombs.
+   * },
+   * player2: {
+   *     id: Session Id.,
+   *     ships: [
+   *         [Ship's occupied points in coordinates format]
+   *     ],
+   *     bombs: Thrown bombs.
+   * }
+   * }*/
+  currentMatches = new Map();
 
-    constructor(sessionManager)
-    {
-        this.sessionManager = sessionManager
+  constructor(sessionManager) {
+    this.sessionManager = sessionManager;
+  }
+
+  /**
+   * Creates a match.
+   * @param player1Id Player 1 session Id.
+   * @param player2Id Player 1 session Id.
+   * @returns {Promise<JSON>} Match unique Id.
+   */
+  create(player1Id, player2Id) {
+    return new Promise((res) => {
+      let uuid = uuidv4();
+      this.currentMatches.set(uuid, {
+        player1: {
+          id: player1Id,
+          ships: [],
+          bombs: [],
+        },
+        player2: {
+          id: player2Id,
+          ships: [],
+          bombs: [],
+        },
+      });
+      res({ success: true, content: uuid });
+    });
+  }
+
+  /**
+   * Fetches a match Id. based on a player Id.
+   * @param playerId Player session Id.
+   * @returns {null|any} Match Id. (returns null if none)
+   */
+  getMatch(playerId) {
+    for (let [id, value] of this.currentMatches.entries()) {
+      if (value.player1.id === playerId || value.player2.id === playerId)
+        return id;
     }
+    return null;
+  }
 
-    /**
-     * Creates a match.
-     * @param player1Id Player 1 session Id.
-     * @param player2Id Player 1 session Id.
-     * @returns {Promise<JSON>} Match unique Id.
-     */
-    create(player1Id, player2Id)
-    {
-        return new Promise(res =>
-        {
-            let uuid = uuidv4()
-            this.currentMatches.set(uuid, {
-                player1: {
-                    id: player1Id,
-                    ships: [],
-                    bombs: []
-                },
-                player2: {
-                    id: player2Id,
-                    ships: [],
-                    bombs: []
-                }
-            })
-            res({success: true, content: uuid})
-        })
-    }
-
-    /**
-     * Fetches a match Id. based on a player Id.
-     * @param playerId Player session Id.
-     * @returns {null|any} Match Id. (returns null if none)
-     */
-    getMatch(playerId)
-    {
-        for (let [id, value] of this.currentMatches.entries())
-        {
-            if (value.player1.id === playerId || value.player2.id === playerId)
-                return id
+  /**
+   * Sets the ships' position in the beginning of a match.
+   * @param matchId Match Id.
+   * @param playerId Player Id.
+   * @param coordinates Array of arrays containing ship positions in a 2-item-array format.
+   * @returns {Promise<JSON>}
+   */
+  setShips(matchId, playerId, coordinates) {
+    return new Promise((res) => {
+      if (this.currentMatches.has(matchId)) {
+        let match = this.currentMatches.get(matchId);
+        if (match.player1.id === playerId) {
+          if (coordinates.length < 8) {
+            this.currentMatches.get(matchId).player1.ships = coordinates;
+            res({ success: true, content: coordinates });
+          } else {
+            res({ success: false, content: "¡El número de barcos no es 8!" });
+          }
+        } else if (match.player2.id === playerId) {
+          if (coordinates.length < 8) {
+            this.currentMatches.get(matchId).player2.ships = coordinates;
+            res({ success: true, content: coordinates });
+          } else {
+            res({ success: false, content: "¡El número de barcos no es 8!" });
+          }
+        } else {
+          res({
+            success: false,
+            content:
+              "¡Acceso prohibido! La partida solicitada no incluye al jugador.",
+          });
         }
-        return null
-    }
+      } else {
+        res({
+          success: false,
+          content: "La partida solicitada es inexistente.",
+        });
+      }
+    });
+  }
 
-    /**
-     * Sets the ships' position in the beginning of a match.
-     * @param matchId Match Id.
-     * @param playerId Player Id.
-     * @param coordinates Array of arrays containing ship positions in a 2-item-array format.
-     * @returns {Promise<JSON>}
-     */
-    setShips(matchId, playerId, coordinates)
-    {
-        return new Promise(res =>
-        {
-            if (this.currentMatches.has(matchId))
-            {
-                let match = this.currentMatches.get(matchId)
-                if (match.player1.id === playerId)
-                {
-                    this.currentMatches.get(matchId).player1.ships = coordinates
-                    res({success: true, content: coordinates})
-                }
-                else if (match.player2.id === playerId)
-                {
-                    this.currentMatches.get(matchId).player2.ships = coordinates
-                    res({success: true, content: coordinates})
-                }
-                else
-                {
-                    res({success: false, content: "¡Acceso prohibido! La partida solicitada no incluye al jugador."})
-                }
+  /**
+   * Throws a bomb into a pair of specified coordinates.
+   * @param matchId Match Id.
+   * @param playerId Player Id.
+   * @param coordinates Coords. where the bomb is thrown.
+   * @returns {Promise<JSON>}
+   */
+  throwBomb(matchId, playerId, coordinates) {
+    return new Promise((res) => {
+      if (this.currentMatches.has(matchId)) {
+        let match = this.currentMatches.get(matchId);
+        if (match.player1.id === playerId) {
+          // Push the bomb's coordinates
+          match.player1.bombs.push(coordinates);
+
+          // Now, iterate over every ship.
+          // Note: Every "ship" is saved as an array of coordinates.
+          match.player2.ships.forEach((shipCoords) => {
+            if (shipCoords.includes(coordinates)) {
+              // Create an intersection between the player's bombs and the ship's coordinates.
+              const damagedShipCoords = shipCoords.filter((value) =>
+                match.player1.bombs.includes(value)
+              );
+
+              // Compare the ship's damaged coordinates with the complete occupation.
+              // Note: If the ship is completely damaged, then they technically would be equal.
+              if (damagedShipCoords === shipCoords) {
+                return res({
+                  success: true,
+                  content: {
+                    coordinates: coordinates,
+                    hasHit: true,
+                    hasSunk: true,
+                  },
+                });
+              } else {
+                return res({
+                  success: true,
+                  content: {
+                    coordinates: coordinates,
+                    hasHit: true,
+                    hasSunk: false,
+                  },
+                });
+              }
             }
-            else
-            {
-                res({success: false, content: "La partida solicitada es inexistente."})
+          });
+
+          // If none of the previous conditions are met, then nothing has actually changed.
+          // Return false values in both cases.
+          return res({
+            success: true,
+            content: {
+              coordinates: coordinates,
+              hasHit: false,
+              hasSunk: false,
+            },
+          });
+        } else if (match.player2.id === playerId) {
+          // It's repetiiitioooon, I'm coooming baaaack tooo yooouuu.
+          match.player2.bombs.push(coordinates);
+
+          match.player1.ships.forEach((shipCoords) => {
+            if (shipCoords.includes(coordinates)) {
+              const damagedShipCoords = shipCoords.filter((value) =>
+                match.player2.bombs.includes(value)
+              );
+
+              if (damagedShipCoords === shipCoords) {
+                return res({
+                  success: true,
+                  content: {
+                    coordinates: coordinates,
+                    hasHit: true,
+                    hasSunk: true,
+                  },
+                });
+              } else {
+                return res({
+                  success: true,
+                  content: {
+                    coordinates: coordinates,
+                    hasHit: true,
+                    hasSunk: false,
+                  },
+                });
+              }
             }
-        })
-    }
+          });
 
-    /**
-     * Throws a bomb into a pair of specified coordinates.
-     * @param matchId Match Id.
-     * @param playerId Player Id.
-     * @param coordinates Coords. where the bomb is thrown.
-     * @returns {Promise<JSON>}
-     */
-    throwBomb(matchId, playerId, coordinates)
-    {
-        return new Promise(res =>
-        {
-            if (this.currentMatches.has(matchId))
-            {
-                let match = this.currentMatches.get(matchId)
-                if (match.player1.id === playerId)
-                {
-                    // Push the bomb's coordinates
-                    match.player1.bombs.push(coordinates)
-
-                    // Now, iterate over every ship.
-                    // Note: Every "ship" is saved as an array of coordinates.
-                    match.player2.ships.forEach(shipCoords =>
-                    {
-                        if (shipCoords.includes(coordinates))
-                        {
-                            // Create an intersection between the player's bombs and the ship's coordinates.
-                            const damagedShipCoords = shipCoords.filter(value => match.player1.bombs.includes(value))
-
-                            // Compare the ship's damaged coordinates with the complete occupation.
-                            // Note: If the ship is completely damaged, then they technically would be equal.
-                            if (damagedShipCoords === shipCoords)
-                            {
-                                return res({
-                                    success: true,
-                                    content: {coordinates: coordinates, hasHit: true, hasSunk: true}
-                                })
-                            }
-                            else
-                            {
-                                return res({
-                                    success: true,
-                                    content: {coordinates: coordinates, hasHit: true, hasSunk: false}
-                                })
-                            }
-                        }
-                    })
-
-                    // If none of the previous conditions are met, then nothing has actually changed.
-                    // Return false values in both cases.
-                    return res({
-                        success: true,
-                        content: {coordinates: coordinates, hasHit: false, hasSunk: false}
-                    })
-                }
-                else if (match.player2.id === playerId)
-                {
-                    // It's repetiiitioooon, I'm coooming baaaack tooo yooouuu.
-                    match.player2.bombs.push(coordinates)
-
-                    match.player1.ships.forEach(shipCoords =>
-                    {
-                        if (shipCoords.includes(coordinates))
-                        {
-                            const damagedShipCoords = shipCoords.filter(value => match.player2.bombs.includes(value))
-
-                            if (damagedShipCoords === shipCoords)
-                            {
-                                return res({
-                                    success: true,
-                                    content: {coordinates: coordinates, hasHit: true, hasSunk: true}
-                                })
-                            }
-                            else
-                            {
-                                return res({
-                                    success: true,
-                                    content: {coordinates: coordinates, hasHit: true, hasSunk: false}
-                                })
-                            }
-                        }
-                    })
-
-                    return res({
-                        success: true,
-                        content: {coordinates: coordinates, hasHit: false, hasSunk: false}
-                    })
-                }
-                else
-                {
-                    res({success: false, content: "¡Acceso prohibido! La partida solicitada no incluye al jugador."})
-                }
-            }
-            else
-            {
-                res({success: false, content: "La partida solicitada es inexistente."})
-            }
-        })
-    }
+          return res({
+            success: true,
+            content: {
+              coordinates: coordinates,
+              hasHit: false,
+              hasSunk: false,
+            },
+          });
+        } else {
+          res({
+            success: false,
+            content:
+              "¡Acceso prohibido! La partida solicitada no incluye al jugador.",
+          });
+        }
+      } else {
+        res({
+          success: false,
+          content: "La partida solicitada es inexistente.",
+        });
+      }
+    });
+  }
 }
 
-module.exports = Match
+module.exports = Match;
+
