7a6c48f8f10dc93f205d1bfa1ee129d52bb25d06
Gerardo Wacker <ge@rar.vg>
2022-06-04 22:34:42 -0300

    Added rounding for the elo calculations (`EloUtil`)

--- src/utils/elo.util.js
+++ src/utils/elo.util.js
@@ -2,33 +2,19 @@ class EloUtil
 {
     static calculateWin(elo1, elo2)
     {
+        const k = 60;
+        let exp = ((elo2 - elo1) / 400)
+        let statistic = 1 / (1 + Math.pow(10, exp))
 
-         const k = 60;
-	let exp = ((elo2 - elo1)/400)
-	let statistic = 1/1+Math.pow(10,exp))
-
-	//si gano se le agrega lo siguiente
-	//1 es por haber ganado
-
-	let result = k*(1-statistic)
-	
-	 
-        return result
+        return Math.round(k * (1 - statistic))
     }
 
-static calculateLoss(elo1, elo2)
+    static calculateLoss(elo1, elo2)
     {
+        const k = 60;
+        let exp = ((elo2 - elo1) / 400)
+        let statistic = 1 / (1 + Math.pow(10, exp))
 
-         const k = 60;
-	let exp = ((elo2 - elo1)/400)
-	let statistic = 1/1+Math.pow(10,exp))
-
-	//0 es por haber perdido
-	let result = k*(0-statistic)
-	 
-        return result
+        return Math.round(k * (0 - statistic))
     }
-
-
-    
 }
